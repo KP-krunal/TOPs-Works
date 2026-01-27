7043e36fb063b2d009234d80f5b98e1fd5652578
KP-krunal <krunaltawar24@gmail.com>
2026-01-27 08:26:42 +0530

    Add files via upload

---
+++ Assignment/studentdata.py
@@ -0,0 +1,14 @@
+name = input("Enter Student Name: ")
+age = int(input("Enter Student Age: "))
+marks = input("Enter Student Marks: ")
+percentage = float(input("Enter Student Percentage: "))
+address = input("Enter Student Address: ")
+
+print("\n**** Student Details ****\n")
+
+print("Student Name: ", name)
+print("Student Age: ", age)
+print("Student Marks: ", marks)
+print("Student Percentage: ", percentage)
+print("Student Address: ", address)
+print("\n*******************")
