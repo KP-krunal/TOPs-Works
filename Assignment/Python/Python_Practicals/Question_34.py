016762ab0b95f0d5978b4174d8628451451c3569
KP-krunal <krunaltawar24@gmail.com>
2026-04-01 10:05:11 +0530

    Refactor list comparison to use user input
    Updated the function to take user input for the lists and check for common members.

--- Assignment/Python/Python_Practicals/Question_34.py
+++ Assignment/Python/Python_Practicals/Question_34.py
@@ -1,16 +1,20 @@
 # Write a Python function that takes two lists and returns true if they
 # have at least one common member.
 
-lst1 = [1, 2, 3, 4, 5,6]
-lst2 = [6, 7, 8, 9, 10]
+# lst1 = [1, 2, 3, 4, 5,6]
+# lst2 = [6, 7, 8, 9, 10]
 
+lst1 = input("Enter the first list : ").split()
+lst2 = input("Enter the second list : ").split()
+print(lst1)
+print(lst2)
+
+found = False
 
 for i in lst1:
     for j in lst2:
         if i == j:
-            print("True")
+            found = True
             break
-    else:
-        continue
-else:
-    print("False")
+    
+print(found)
